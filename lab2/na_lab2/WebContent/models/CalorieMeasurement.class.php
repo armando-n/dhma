8bde5dee38864fc836bdd879a32f729ae9cbae56
armando-n <armando.n@outlook.com>
2015-09-27 03:29:35 -0500

    Added skeletons for measurement model classes

---
+++ lab2/na_lab2/WebContent/models/CalorieMeasurement.class.php
@@ -0,0 +1,86 @@
+<?php
+class CalorieMeasurement {
+    
+    private $formInput;
+    private $errors;
+    private $errorCount;
+    private $date;
+    private $time;
+    private $calories;
+    
+    public function __construct($formInput = null) {
+        $this->formInput = $formInput;
+        Messages::reset();
+        $this->initialize();
+    }
+    
+    public function getError($errorName) {
+        if (isset($this->errors[$errorName]))
+            return $this->errors[$errorName];
+    
+        return "";
+    }
+    
+    public function setError($errorName, $errorValue) {
+        $this->errors[$errorName] =  Messages::getError($errorValue);
+        $this->errorCount++;
+    }
+    
+    public function getErrors() {
+        return $this->errors;
+    }
+    
+    public function getErrorCount() {
+        return $this->errorCount;
+    }
+    
+    public function getDate() {
+        return $this->date;
+    }
+    
+    public function getTime() {
+        return $this->time;
+    }
+    
+    public function getMeasurement() {
+        return $this->calories;
+    }
+    
+    public function __toString() {
+        $str =
+            "Date: [" . $this->date . "]\n" .
+            "Time: [" . $this->time . "]\n" .
+            "Calories Consumed: [" . $this->calories . "]";
+        
+        return $str;
+    }
+    
+    private function initialize() {
+        $this->errors = array();
+        $this->errorCount = 0;
+        
+        if (is_null($this->formInput)) {
+            $this->date = '';
+            $this->time = '';
+            $this->calories = '';
+        } else {
+            $this->validateDate();
+            $this->validateTime();
+            $this->validateMeasurement();
+        }
+    }
+    
+    private function validateDate() {
+    
+    }
+    
+    private function validateTime() {
+    
+    }
+    
+    private function validateMeasurement() {
+    
+    }
+
+}
+?>
