f6ae44949f50e27cb50d70a026db075908aa6d2f
armando-n <armando.n@outlook.com>
2015-09-28 00:16:41 -0500

    Added/completed SleepMeasurement model/tests

--- lab2/na_lab2/WebContent/models/SleepMeasurement.class.php
+++ lab2/na_lab2/WebContent/models/SleepMeasurement.class.php
@@ -4,9 +4,9 @@ class SleepMeasurement {
     private $formInput;
     private $errors;
     private $errorCount;
-    private $date;
-    private $time;
-    private $sleepDuration;
+    private $userName;
+    private $datetime;
+    private $duration;
     
     public function __construct($formInput = null) {
         $this->formInput = $formInput;
@@ -34,23 +34,42 @@ class SleepMeasurement {
         return $this->errorCount;
     }
     
+    public function getUserName() {
+        return $this->userName;
+    }
+    
+    public function getDateTime() {
+        return $this->datetime;
+    }
+    
     public function getDate() {
-        return $this->date;
+        return is_object($this->datetime) ? $this->datetime->format("Y-m-d") : '';
     }
     
     public function getTime() {
-        return $this->time;
+        return is_object($this->datetime) ? $this->datetime->format("h:i:s a") : '';
     }
     
     public function getMeasurement() {
-        return $this->sleepDuration;
+        return $this->duration;
+    }
+    
+    public function getParameters() {
+        $params = array(
+                "userName" => $this->userName,
+                "datetime" => $this->datetime,
+                "duration" => $this->duration
+        );
+    
+        return $params;
     }
     
     public function __toString() {
+        $dtVal = is_object($this->datetime) ? $this->datetime->format("Y-m-d h:i:s a") : '';
         $str =
-            "Date: [" . $this->date . "]\n" .
-            "Time: [" . $this->time . "]\n" .
-            "Sleep duration: [" . $this->sleepDuration . "]";
+            "User Name: [" .$this->userName . "]\n" .
+            "Date and Time: [" . $dtVal . "]\n" .
+            "Sleep Duration: [" . $this->duration . "]";
         
         return $str;
     }
@@ -60,26 +79,86 @@ class SleepMeasurement {
         $this->errorCount = 0;
         
         if (is_null($this->formInput)) {
-            $this->date = '';
-            $this->time = '';
-            $this->sleepDuration = '';
+            $this->userName = '';
+            $this->datetime = '';
+            $this->duration = '';
         } else {
-            $this->validateDate();
-            $this->validateTime();
+            $this->validateUserName();
+            $this->validateDateAndTime();
             $this->validateMeasurement();
         }
     }
     
-    private function validateDate() {
+    private function validateUserName() {
+        $this->userName = Utilities::extractForm($this->formInput, "userName");
+        if (empty($this->userName)) {
+            $this->setError("userName", "USER_NAME_EMPTY");
+            return;
+        }
     
+        if (strlen($this->userName) > 15) {
+            $this->setError("userName", "USER_NAME_TOO_LONG");
+            return;
+        }
+    
+        $options = array("options" => array("regexp" => "/^[a-zA-Z0-9_-]+$/"));
+        if (!filter_var($this->userName, FILTER_VALIDATE_REGEXP, $options)) {
+            $this->setError("userName", "USER_NAME_HAS_INVALID_CHARS");
+            return;
+        }
     }
     
-    private function validateTime() {
+    private function validateDateAndTime() {
+        $date = Utilities::extractForm($this->formInput, "date");
+        $time = Utilities::extractForm($this->formInput, "time");
+        $this->datetime = '';
     
+        if (empty($date)) {
+            $this->setError("datetime", "DATE_EMPTY");
+            return;
+        }
+    
+        if (empty($time)) {
+            $this->setError("datetime", "TIME_EMPTY");
+            return;
+        }
+    
+        $options = array("options" => array("regexp" => "/^((\d{4}[\/-]\d\d[\/-]\d\d)|(\d\d[\/-]\d\d[\/-]\d{4}))$/"));
+        if (!filter_var($date, FILTER_VALIDATE_REGEXP, $options)) {
+            $this->setError("datetime", "DATE_HAS_INVALID_CHARS");
+            return;
+        }
+    
+        $options = array("options" => array("regexp" => "/^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])(:([0-5]?[0-9]))?$/"));
+        if (!filter_var($time, FILTER_VALIDATE_REGEXP, $options)) {
+            $this->setError("datetime", "TIME_HAS_INVALID_CHARS");
+            return;
+        }
+    
+        try { $dt = new DateTime($date . ' ' . $time); }
+        catch (Exception $e) {
+            $this->setError("datetime", "DATE_AND_TIME_INVALID");
+            return;
+        }
+    
+        $this->datetime = $dt;
     }
     
     private function validateMeasurement() {
-    
+        $this->duration = Utilities::extractForm($this->formInput, "duration");
+        
+        if (empty($this->duration)) {
+            $this->setError("duration", "DURATION_EMPTY");
+            return;
+        }
+        
+        $options = array("options" => array("regexp" => "/^\d+$/"));
+        if (!filter_var($this->duration, FILTER_VALIDATE_REGEXP, $options)) {
+            $this->setError("duration", "DURATION_HAS_INVALID_CHARS");
+            return;
+        }
+        
+        $this->duration = (int)$this->duration;
     }
 
 }
