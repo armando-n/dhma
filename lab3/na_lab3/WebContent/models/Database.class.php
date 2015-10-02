51e006ed074c610f9fc4190951d29b9d06a467a8
armando-n <armando.n@outlook.com>
2015-10-02 14:11:44 -0500

    Added Database class/tests

---
+++ lab3/na_lab3/WebContent/models/Database.class.php
@@ -0,0 +1,28 @@
+<?php
+class Database {
+    
+    private static $db;
+    
+    public static function getDB($dbName = 'dhma', $configFile = 'myConfig.ini') {
+        if (!isset(Database::$db)) {
+            try {
+                // read database config info from file
+                $configArray = parse_ini_file('../../../' . $configFile);
+                if ($configArray === FALSE)
+                    throw new Exception("blahblah");
+                $userName = $configArray["username"];
+                $pass = $configArray["password"];
+                
+                // open the connection
+                Database::$db = new PDO("mysql:host=localhost;dbname=$dbName;charset=utf8", $userName, $pass);
+                Database::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $e) {
+                echo "Failed to connect to database $dbName: " . $e->getMessage();
+            }
+        }
+        
+        return Database::$db;
+    }
+}
+?>
