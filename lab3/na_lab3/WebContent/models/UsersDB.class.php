7214027e646cb166a1ebdaadef0181d0b17a6949
armando-n <armando.n@outlook.com>
2015-10-11 20:18:48 -0500

    Added UsersDBTest; Added methods to UsersDB;

--- lab3/na_lab3/WebContent/models/UsersDB.class.php
+++ lab3/na_lab3/WebContent/models/UsersDB.class.php
@@ -5,6 +5,9 @@ class UsersDB {
     public static function addUser($user, $dbName = null, $configFile = null) {
         $returnUserID = -1;
         
+        if (!($user instanceof User))
+            throw new InvalidArgumentException("Error: Not valid User object");
+        
         try {
             $db = Database::getDB($dbName, $configFile);
             $stmt = $db->prepare(
@@ -53,7 +56,7 @@ class UsersDB {
     
     // returns a User object whose $type field has value $value
     public static function getUserBy($type, $value, $dbName = null, $configFile = null) {
-        $allowed = array('userID', 'userName');
+        $allowed = array('userID', 'userName', 'dateCreated');
         $user = null;
         
         try {
@@ -77,6 +80,78 @@ class UsersDB {
         return $user;
     }
     
+    public static function getAllUsersSortedByDateCreated($order, $dbName = null, $configFile = null) {
+        $allowedOrders = array('asc', 'desc');
+        $allUsers = array();
+        
+        try {
+            if (!in_array($order, $allowedOrders))
+                throw new Exception("$order is not an allowed order");
+            
+            $db = Database::getDB($dbName, $configFile);
+            $stmt = $db->prepare("select * from Users order by dateCreated $order");
+            $stmt->execute();
+        
+            foreach ($stmt as $row) {
+                $user = new User($row);
+                if (!is_object($user) || !empty($user->getErrors()))
+                    throw new PDOException("Failed to create valid user");
+        
+                $allUsers[] = $user;
+            }
+        
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        } catch (RuntimeException $e) {
+            echo $e->getMessage();
+        }
+        
+        return $allUsers;
+    }
+    
+    // returns an array of User objects whose created since the specified date string
+    public static function getUsersCreatedSince($dateString, $dbName = null, $configFile = null) {
+        return UsersDB::getUsersByDate($dateString, 'after', $dbName, $configFile);
+    }
+    
+    // returns an array of User objects whose created by the specified date string
+    public static function getUsersCreatedBy($dateString, $dbName = null, $configFile = null) {
+        return UsersDB::getUsersByDate($dateString, 'before', $dbName, $configFile);
+    }
+    
+    private static function getUsersByDate($dateString, $direction, $dbName = null, $configFile = null) {
+        $allowedDirections = array('before', 'after');
+        $users = array();
+    
+        try {
+            if (!in_array($direction, $allowedDirections))
+                throw new PDOException("$direction is not an allowed direction");
+            $operator = ($direction === 'before') ? '<=' : '>=';
+            
+            $datetime = new DateTime($dateString);
+            $db = Database::getDB($dbName, $configFile);
+            $stmt = $db->prepare("select * from Users where dateCreated $operator :date");
+            $stmt->execute(array(":date" => $datetime->format('Y-m-d')));
+    
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if ($rows !== false)
+                foreach ($rows as $row)
+                    $users[] = new User($row);
+    
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        } catch (RuntimeException $e) {
+            echo $e->getMessage();
+        } catch (Exception $e) {
+            if (stristr($e->getMessage(), 'Failed to parse') !== false)
+                throw new Exception("Invalid date: $dateString");
+            else
+                throw $e;
+        }
+    
+        return $users;
+    }
+    
     public static function addUserTest($user) {
         return UsersDB::addUser($user, 'dhma_testDB', 'myConfig.ini');
     }
@@ -87,6 +162,18 @@ class UsersDB {
     
     public static function getUserByTest($type, $value) {
         return UsersDB::getUserBy($type, $value, 'dhma_testDB', 'myConfig.ini');
+    }
+    
+    public static function getUsersCreatedSinceTest($dateString) {
+        return UsersDB::getUsersCreatedSince($dateString, 'dhma_testDB', 'myConfig.ini');
+    }
+    
+    public static function getUsersCreatedByTest($dateString) {
+        return UsersDB::getUsersCreatedBy($dateString, 'dhma_testDB', 'myConfig.ini');
+    }
+    
+    public static function getAllUsersSortedByDateCreatedTest($order) {
+        return UsersDB::getAllUsersSortedByDateCreated($order, 'dhma_testDB', 'myConfig.ini');
     }
 }
 ?>
---
+++ lab3/na_lab3/unit/models/UsersDBTest.php
@@ -0,0 +1,262 @@
+<?php
+require_once dirname(__FILE__) . '\..\..\WebContent\models\Database.class.php';
+require_once dirname(__FILE__) . '\..\..\WebContent\models\GenericModelObject.class.php';
+require_once dirname(__FILE__) . '\..\..\WebContent\models\User.class.php';
+require_once dirname(__FILE__) . '\..\..\WebContent\models\UserProfile.class.php';
+require_once dirname(__FILE__) . '\..\..\WebContent\models\UsersDB.class.php';
+require_once dirname(__FILE__) . '\..\..\WebContent\models\Messages.class.php';
+
+class UsersDBTest extends PHPUnit_Framework_TestCase {
+    
+    /** @expectedException Exception
+     */
+    public function testAddUserWithNoParameters() {
+        UsersDB::addUserTest();
+    }
+    
+    public function testAddUserWithUserParameter() {
+        $input = array(
+            "userName" => "nathan-m",
+            "password" => "password123",
+            "password1" => "password123",
+            "password2" => "password123"
+        );
+        $user = new User($input);
+        $this->dbQuery("delete from Users where userName = 'nathan-m'");
+        
+        $rowsBeforeAdd = $this->dbSelect("select * from Users where userName = 'nathan-m'");
+        $userID = UsersDB::addUserTest($user);
+        $rowsAfterAdd = $this->dbSelect("select * from Users where userName = 'nathan-m'");
+        
+        $this->assertEmpty($rowsBeforeAdd,
+            'It should not have the user "nathan-m" before the user has been added');
+        $this->assertCount(1, $rowsAfterAdd,
+            'It should have a new row in the Users table of the database when the user parameter is provided');
+        $this->assertArrayHasKey('userName', $rowsAfterAdd[0],
+            'It should have a new row in the Users table of the database when the user parameter is provided');
+        $this->assertEquals("nathan-m", $rowsAfterAdd[0]["userName"],
+            'It should have a new row in the Users table of the database when the user parameter is provided');
+        $this->assertTrue(is_numeric($userID),
+            'It should return the user ID of the added user when the user parameter is provided');
+        
+    }
+    
+    /** @expectedException InvalidArgumentException
+     */
+    public function testAddUserWithInvalidUserParameter() {
+        $profile = new UserProfile();
+        UsersDB::addUserTest($profile);
+    }
+    
+    public function testGetAllUsers() {
+        $users = UsersDB::getAllUsersTest();
+        
+        $this->assertGreaterThan(0, count($users),
+            'It should call getAllUsers and return an array with at least 1 element');
+        
+        foreach ($users as $user) {
+            $this->assertInstanceOf('User', $user,
+                'It should call getAllUsers and return an array of User objects');
+            $this->assertEmpty($user->getErrors(),
+                'It should call getAllUsers and not have errors in the returned User objects');
+            $this->assertEquals(0, $user->getErrorCount(),
+                'It should call getAllUsers and have an error count of 0 in the returned User objects');
+        }
+    }
+    
+    public function testGetAllUsersWithNullParameter() {
+        $users = UsersDB::getAllUsersTest(null);
+        
+        $this->assertGreaterThan(0, count($users),
+            'It should call getAllUsers with a null parameter and return an array with at least 1 element');
+        
+        foreach ($users as $user) {
+            $this->assertInstanceOf('User', $user,
+                'It should call getAllUsers with a null parameter and return an array of User objects');
+            $this->assertEmpty($user->getErrors(),
+                'It should call getAllUsers with a null parameter and not have errors in the returned User objects');
+            $this->assertEquals(0, $user->getErrorCount(),
+                'It should call getAllUsers with a null parameter and have an error count of 0 in the returned User objects');
+        }
+    }
+    
+    /** @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testGetUserByWithNoParameters() {
+        $user = UsersDB::getUserByTest();
+    }
+    
+    /** @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testGetUserByWithNoValueParameter() {
+        $user = UsersDB::getUserByTest('userName');
+    }
+    
+    public function testGetUserByWithValidParameters() {
+        $user = UsersDB::getUserByTest('userName', 'armando-n');
+        
+        $this->assertInstanceOf('User', $user,
+            'It should return a User object when valid parameters are provided');
+        $this->assertEquals('armando-n', $user->getUserName(),
+            'It should return a User object with the provided user name when valid parameters are provided');
+    }
+    
+    public function testGetUserByWithUnknownUser() {
+        $user = UsersDB::getUserByTest('userName', 'unkown-user');
+    
+        $this->assertNull($user,
+            'It should return NULL when an unknown user name is provided');
+    }
+    
+    public function testGetUsersCreatedSinceWithValidDateAndResults() {
+        $users = UsersDB::getUsersCreatedSinceTest('2015-10-11');
+        
+        $this->assertNotNull($users,
+            'It should call getUsersCreatedSince and return an array when a valid date is provided');
+        $this->assertGreaterThan(0, count($users),
+            'It should call getUsersCreatedSince and return a non-empty array when a valid date is provided');
+        
+        foreach ($users as $user) {
+            $this->assertInstanceOf('User', $user,
+                'It should call getUsersCreatedSince and return an array of User objects when a valid date is provided');
+            $this->assertEmpty($user->getErrors(),
+                'It should call getUsersCreatedSince and not have errors in the returned User objects when a valid date is provided');
+            $this->assertEquals(0, $user->getErrorCount(),
+                'It should call getUsersCreatedSince and have an error count of 0 in the returned User objects when a valid date is provided');
+        }
+    }
+    
+    public function testGetUsersCreatedSinceWithInvalidDateAndNoResults() {
+        $users = UsersDB::getUsersCreatedSinceTest('2050-10-11');
+    
+        $this->assertNotNull($users,
+            'It should call getUsersCreatedSince and return an array when an invalid date is provided');
+        $this->assertCount(0, $users,
+            'It should call getUsersCreatedSince and return an empty array when an invalid date is provided');
+    }
+    
+    /** @expectedException Exception
+     *  @expectedExceptionMessage Invalid date
+     */
+    public function testGetUsersCreatedSinceWithInvalidDateString() {
+        $users = UsersDB::getUsersCreatedSinceTest('invalid date string');
+    }
+    
+    public function testGetUsersCreatedByWithValidDateAndResults() {
+        $users = UsersDB::getUsersCreatedByTest('2015-10-05');
+    
+        $this->assertNotNull($users,
+            'It should call getUsersCreatedBy and return an array when a valid date is provided');
+        $this->assertGreaterThan(0, count($users),
+            'It should call getUsersCreatedBy and return a non-empty array when a valid date is provided');
+    
+        foreach ($users as $user) {
+            $this->assertInstanceOf('User', $user,
+                'It should call getUsersCreatedBy and return an array of User objects when a valid date is provided');
+            $this->assertEmpty($user->getErrors(),
+                'It should call getUsersCreatedBy and not have errors in the returned User objects when a valid date is provided');
+            $this->assertEquals(0, $user->getErrorCount(),
+                'It should call getUsersCreatedBy and have an error count of 0 in the returned User objects when a valid date is provided');
+        }
+    }
+    
+    public function testGetUsersCreatedByWithInvalidDateAndNoResults() {
+        $users = UsersDB::getUsersCreatedByTest('1950-10-05');
+    
+        $this->assertNotNull($users,
+            'It should call getUsersCreatedBy and return an array when an invalid date is provided');
+        $this->assertCount(0, $users,
+            'It should call getUsersCreatedBy and return an empty array when an invalid date is provided');
+    }
+    
+    /** @expectedException Exception
+     *  @expectedExceptionMessage Invalid date
+     */
+    public function testGetUsersCreatedByWithInvalidDateString() {
+        $users = UsersDB::getUsersCreatedByTest('invalid date string');
+    }
+    
+    /** @expectedException Exception
+     *  @expectedExceptionMessage Missing argument
+     */
+    public function testGetAllUsersSortedByDateCreatedWithNoParameters() {
+        UsersDB::getAllUsersSortedByDateCreatedTest();
+    }
+    
+    public function testGetAllUsersSortedByDateCreatedAscending() {
+        $users = UsersDB::getAllUsersSortedByDateCreatedTest('asc');
+        
+        $this->assertGreaterThan(0, count($users),
+            'It should call getAllUsersSortedByDateCreated and return an array with at least 1 element when a valid order is provided');
+        $previousDate = new DateTime('1950-01-01');
+        
+        foreach ($users as $user) {
+            $this->assertInstanceOf('User', $user,
+                'It should call getAllUsersSortedByDateCreated and return an array of User objects when a valid order is provided');
+            $this->assertEmpty($user->getErrors(),
+                'It should call getAllUsersSortedByDateCreated and not have errors in the returned User objects when a valid order is provided');
+            $this->assertEquals(0, $user->getErrorCount(),
+                'It should call getAllUsersSortedByDateCreated and have an error count of 0 in the returned User objects when a valid order is provided');
+//             $this->assertGreaterThan($previousDate, $user->getDateTime(),
+//                 'It should call getAllUsersSortedByDateCreated and return array of User objects in ascending date order');
+        }
+    }
+    
+    public function testGetAllUsersSortedByDateCreatedDescending() {
+        $users = UsersDB::getAllUsersSortedByDateCreatedTest('desc');
+    
+        $this->assertGreaterThan(0, count($users),
+            'It should call getAllUsersSortedByDateCreated and return an array with at least 1 element when a valid order is provided');
+        $previousDate = new DateTime('1950-01-01');
+    
+        foreach ($users as $user) {
+            $this->assertInstanceOf('User', $user,
+                'It should call getAllUsersSortedByDateCreated and return an array of User objects when a valid order is provided');
+            $this->assertEmpty($user->getErrors(),
+                'It should call getAllUsersSortedByDateCreated and not have errors in the returned User objects when a valid order is provided');
+            $this->assertEquals(0, $user->getErrorCount(),
+                'It should call getAllUsersSortedByDateCreated and have an error count of 0 in the returned User objects when a valid order is provided');
+//             $this->assertGreaterThan($previousDate, $user->getDateTime(),
+//                 'It should call getAllUsersSortedByDateCreated and return array of User objects in ascending date order');
+        }
+    }
+    
+    /** @expectedException Exception
+     *  @expectedExceptionMessage not an allowed order
+     */
+    public function testGetAllUsersSortedByDateCreatedWithInvalidOrder() {
+        $users = UsersDB::getAllUsersSortedByDateCreatedTest('invalid order');
+    }
+    
+    private function dbQuery($query, $dbName = null, $configFile = null) {
+        try {
+            $db = Database::getDB($dbName, $configFile);
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        } catch (RuntimeException $e) {
+            echo $e->getMessage();
+        }
+    }
+    
+    private function dbSelect($query, $dbName = null, $configFile = null) {
+        try {
+            $db = Database::getDB($dbName, $configFile);
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt->closeCursor();
+        
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        } catch (RuntimeException $e) {
+            echo $e->getMessage();
+        }
+        
+        return $rows;
+    }
+    
+}
+?>
