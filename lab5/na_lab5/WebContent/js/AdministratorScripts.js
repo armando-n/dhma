2ed8efba98725df136429863eec259567715267a
armando-n <armando.n@outlook.com>
2015-11-11 09:19:18 -0600

    Little more work on AdministratorScripts

--- lab5/na_lab5/WebContent/js/AdministratorScripts.js
+++ lab5/na_lab5/WebContent/js/AdministratorScripts.js
@@ -17,28 +17,53 @@ $(document).ready(function() {
 	var membersTable = $('#membertable').DataTable( {
 		scrollY: '35vh',
 		scrollCollapse: true,
-		paging: false,
-		info: false,
+		//paging: false,
+		//info: false,
 		select: true,
-		buttons: [
-		    {
-		    	extend: 'selected',
-		    	text: 'This is a test',
-		    	action: function (e, dt, button, config) {
-		    		var rows = dt.rows( { selected: true } ).count();
-		    	    alert( 'There are ' +rows+ '(s) selected in the table');
-		    	}
-		    }
-        ]
+		dom: 'ftB',
+		buttons: {
+			name: 'primary',
+			buttons: [
+			    {
+			    	extend: 'selectedSingle',
+			    	text: 'Edit',
+			    	action: editMember
+			    },
+			    {
+			    	extend: 'selected',
+			    	text: 'Delete',
+			    	action: deleteMember
+			    },
+			    'columnsToggle'
+			]
+//	buttons: [
+//    {
+//    	extend: 'selectedSingle',
+//    	text: 'Edit',
+//    	action: editMember
+//    },
+//    {
+//    	extend: 'selected',
+//    	text: 'Delete',
+//    	action: deleteMember
+//    },
+		}
+	
 	} );
 	
 	// add a button to the table
-	membersTable.buttons().container().appendTo(membersTable.table().container());
+//	membersTable.buttons().container().appendTo(membersTable.table().container());
 });
 
-function rowSelected(e, dt, node, config) {
+function editMember(e, dt, button, config) {
 	var rows = dt.rows( { selected: true } ).count();
-    alert( 'There are ' +rows+ '(s) selected in the table');
+	var rowData = dt.rows( { selected: true } ).data();
+    alert( 'You have chosen to edit the member: ' +rowData.Name);
+}
+
+function deleteMember(e, dt, button, config) {
+	var rows = dt.rows( { selected: true } ).count();
+	alert('You have chosen to delete ' +rows+ ' member(s)');
 }
 	
 })();
