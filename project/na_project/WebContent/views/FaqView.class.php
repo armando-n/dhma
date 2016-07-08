c4bb8aed2c029aeb79686b046d262c24de5d8486
Armando <armando.n@outlook.com>
2016-07-08 14:45:31 -0500

    Fixed FAQ appearance and typo

--- project/na_project/WebContent/views/FaqView.class.php
+++ project/na_project/WebContent/views/FaqView.class.php
@@ -25,33 +25,33 @@ class FaqView {
 
         <h2 class="hidden">Questions and Answers</h2>
 
-        <p id="use">
-            <span class="question">Should I use this site?</span>
-            Not yet. You can play with it if you like, but the data is deleted regularly.
+        <h3 id="use">Should I use this site?</h3>
+        <p>
+            Not yet. You can play with it if you'd like, but the data is deleted regularly.
             This site is still going through too many changes.
             Once this site is ready for use, I will update this answer.
         </p>
 
-        <p id="purpose">
-            <span class="question">Why does this site exist?</span>
+        <h3 id="purpose">Why does this site exist?</h3>
+        <p>
             This site was created for an assignment in a Web Technologies course I took while studying to earn a degree in Computer Science.
             Although it's not fully fleshed out yet, it is on the internet so that potential employers and the curious can check it out.
         </p>
     
-        <p id="function">
-            <span class="question">What does this site do?</span>
+        <h3 id="function">What does this site do?</h3>
+        <p>
             The purpose of this site is to provide a health tracking tool that targets things that people with diabetes may find helpful.
             Once you sign up, you can keep track of various measurements from any device.
         </p>
         
-        <p id="privacy">
-            <span class="question">Who can see my data?</span>
+        <h3 id="privacy">Who can see my data?</h3>
+        <p>
             Your measurement data is always kept private. Your profile information and profile picture can eventually be set to private,
             once that feature has been implemented. For now, it is always public.
         </p>
         
-        <p id="future">
-            <span class="question">Is there more to this site than keeping track of measurements?</span>
+        <h3 id="future">Is there more to this site than keeping track of measurements?</h3>
+        <p>
             The measurement tracking functions are all that are currently provided. In the short term, more health tracking features
             and measurements will be added. In the distant future, I hope to allow you to enhance the calorie tracking function to
             allow you to create meal plans, specify the calorie content of foods you eat, and share this information with other memembers.
